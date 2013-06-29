205434f8eeb50552b37b95585c5fe3bbfb2e85a1
Weber <weberdls@gmail.com>
2013-06-29 15:25:43 -0300

    arrumando cagada

--- src/br/com/caelum/cadastro/ListaAlunos.java
+++ src/br/com/caelum/cadastro/ListaAlunos.java
@@ -19,9 +19,12 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ListView;
+import android.widget.Toast;
 import br.com.caelum.cadastro.adapter.ListaAlunosAdapter;
 import br.com.caelum.cadastro.dao.AlunoDAO;
 import br.com.caelum.cadastro.modelo.Aluno;
+import br.com.caelum.cadastro.util.AlunoConverter;
+import br.com.caelum.cadastro.util.WebClient;
 
 public class ListaAlunos extends Activity {
 
@@ -194,7 +197,17 @@ public class ListaAlunos extends Activity {
 			Intent irParaFormulario = new Intent(this, Formulario.class);
 			startActivity(irParaFormulario);
 			break;
-
+		case R.id.enviar_alunos:
+			String urlServidor = "http://caelum.com.br/mobile";
+			AlunoDAO dao = new AlunoDAO(this);
+			List<Aluno> alunos = dao.getLista();
+			dao.close();
+	
+			String dadosJson = new AlunoConverter().toJSON(alunos);
+			WebClient client = new WebClient(urlServidor);
+			String respostaJson = client.post(dadosJson);
+			Toast.makeText(this, respostaJson, Toast.LENGTH_LONG).show();
+			break;
 		default:
 			break;
 		}
